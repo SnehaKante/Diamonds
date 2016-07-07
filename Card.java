ba4c4777100966b429f96f8abc2cf09acf5799d5
C.S.Sowmya <sowmya.chandrashaker@cdk.com>
2016-07-07 15:32:33 +0530

    implemented getSuit() and displayCard()

--- Card.java
+++ Card.java
@@ -1,4 +1,5 @@
-public class Card {
+
+ class Card {
 	
 	int seq;
 	
@@ -7,17 +8,28 @@ public class Card {
 	}
 	
 	public char getPip() {
-	String pips = "A23456789TJQK";
-	return pips.charAt(seq % 13);
+		String pips = "A23456789TJQK";
+		return pips.charAt(this.seq % 13);
 	}
 	
+	public String getSuit() {
+		String[] cdhs = { "club", "diamond", "heart", "spade" };
+		return cdhs[this.seq / 13];
+	}
+	
+	public String displayCard() {
+	    return getPip()+" of "+getSuit();
+	}
 	
 	
 	
 	public static void main(String args[]) {
 		Card c = new Card(15);
-		System.out.println(c.getPip());
+		System.out.println(c.displayCard());
+		
 		
 	}
 	
 } 
+	
+	
