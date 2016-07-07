f944a7c21e0cb9ab301935b87140f54ea0f7705a
VivekM <mvivek95@hotmail.com>
2016-07-07 15:02:26 +0530

    implemented getPip() method

---
+++ Card.java
@@ -0,0 +1,22 @@
+public class Card {
+	
+	int seq;
+	
+	public Card (int seq) {
+		this.seq = seq;
+	}
+	
+	public char getPip() {
+	String pips = "A23456789TJQK";
+	return pips.charAt(seq % 13);
+	}
+	
+	
+	
+	public static void main(String args[]) {
+		Card c = new Card(15);
+		System.out.println(c.getPip());
+		
+	}
+	
+} 
