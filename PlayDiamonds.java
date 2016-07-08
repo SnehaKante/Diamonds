8d459f02fed6bdc8e6afc6a8522a8736ee3cad66
VivekM <mvivek95@hotmail.com>
2016-07-08 10:01:38 +0530

    Added Bidwinner method

--- PlayDiamonds.java
+++ PlayDiamonds.java
@@ -3,6 +3,9 @@ import java.util.Collections;
 
 
 public class PlayDiamonds {
+	
+	
+	public ArrayList list;
 
 	public String shuffleDiamondCards() {
 		ArrayList diamondStack = new ArrayList();
@@ -36,17 +39,51 @@ public class PlayDiamonds {
 	}
 	
 	public String winner(int userBidCardSeq) {
-		if (new PlayDiamonds().computerBid().charAt(0) > new PlayDiamonds().userBid(userBidCardSeq).charAt(0) )
+		int cardPipValue1 = 0;
+		if(new PlayDiamonds().computerBid().charAt(0) == 'A')
+			cardPipValue1 = 1;
+		else if(new PlayDiamonds().computerBid().charAt(0) == 'T')
+			cardPipValue1 = 10;	
+		else if(new PlayDiamonds().computerBid().charAt(0) == 'J')
+			cardPipValue1 = 11;	
+		else if(new PlayDiamonds().computerBid().charAt(0) == 'Q')
+			cardPipValue1 = 12;
+		else if(new PlayDiamonds().computerBid().charAt(0) == 'K')
+			cardPipValue1 = 13;
+		else cardPipValue1 = (int)(new PlayDiamonds().computerBid().charAt(0));	
+			
+		int cardPipValue2 = 0;
+		if(new PlayDiamonds().computerBid().charAt(0) == 'A')
+			cardPipValue2 = 1;
+		else if(new PlayDiamonds().userBid(userBidCardSeq).charAt(0) == 'T')
+			cardPipValue2 = 10;	
+		else if(new PlayDiamonds().userBid(userBidCardSeq).charAt(0) == 'J')
+			cardPipValue2 = 11;	
+		else if(new PlayDiamonds().userBid(userBidCardSeq).charAt(0) == 'Q')
+			cardPipValue2 = 12;
+		else if(new PlayDiamonds().userBid(userBidCardSeq).charAt(0) == 'K')
+			cardPipValue2 = 13;	
+		else cardPipValue2 = (int) (new PlayDiamonds().userBid(userBidCardSeq).charAt(0));	
+				
+		
+		if (cardPipValue1 > cardPipValue2 )
 			return "Computer wins the bid";
 		else
 			return "User wins the bid";
 	}
+	
+	public boolean isGameOver(ArrayList computerCards, ArrayList userCards) {
+		return computerCards.isEmpty() && userCards.isEmpty();
+	}
+	
+	
 	
 	public static void main(String[] args) {
 		System.out.println(new PlayDiamonds().computerBid());
 		System.out.println(new PlayDiamonds().shuffleDiamondCards());
 		System.out.println(new PlayDiamonds().userBid(31));
 		System.out.println(new PlayDiamonds().winner(31));
+		
 	}
 
 }
